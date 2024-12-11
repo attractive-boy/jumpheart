d98756900129339106905fd15adc8a7777d5ed5a
李成 <2414687096@qq.com>
2024-12-11 10:47:16 +0800

    1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Layout } from '@/components/Layout'
 
 export default function App({ Component, pageProps }: AppProps) {
   // 可以通过页面组件的自定义属性来控制是否显示标签栏
   const showTabBar = (Component as any).showTabBar
 
   return (
-    <Layout showTabBar={showTabBar}>
       <Component {...pageProps} />
-    </Layout>
   )
 }
