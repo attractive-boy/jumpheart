958595319443a807a7d05edc7e9a0cde1289fe6f
李成 <2414687096@qq.com>
2024-12-27 16:02:46 +0800

    1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 
 export default function Home() {
-  
   const [isPlaying, setIsPlaying] = useState(false);
   const videoRef = useRef<HTMLVideoElement | null>(null);
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
@@ -10,50 +9,68 @@ export default function Home() {
   // 添加播放控制函数
   const togglePlay = () => {
     if (videoRef.current) {
-      if (!isPlaying) {
-        if (videoRef.current && canvasRef.current) {
-          const context = canvasRef.current.getContext('2d');
-          videoRef.current.loop = true;
-          videoRef.current.play();
-    
-          // 设置canvas的宽高与视频一致
-          canvasRef.current.width = videoRef.current.videoWidth;
-          canvasRef.current.height = videoRef.current.videoHeight;
-    
-          const draw = () => {
-            if (context && videoRef.current && canvasRef.current) {
-              context.drawImage(videoRef.current, 0, 0, canvasRef.current.width, canvasRef.current.height);
-            }
-            requestAnimationFrame(draw);
-          };
-    
-          draw();
-        }
-        setIsPlaying(true); // 更新播放状态
-      }
+      videoRef.current.muted = !isPlaying; // 切换静音状态
+      setIsPlaying(!isPlaying);
     }
   };
 
+  // 播放视频到canvas
+  useEffect(() => {
+    if (videoRef.current && canvasRef.current) {
+      const context = canvasRef.current.getContext('2d');
+      videoRef.current.loop = true;
+      videoRef.current.play();
+
+      // 设置canvas的宽高与视频一致
+      canvasRef.current.width = videoRef.current.videoWidth;
+      canvasRef.current.height = videoRef.current.videoHeight;
+
+      const draw = () => {
+        if (context && videoRef.current && canvasRef.current) {
+          context.drawImage(videoRef.current, 0, 0, canvasRef.current.width, canvasRef.current.height);
+        }
+        requestAnimationFrame(draw);
+      };
+
+      draw();
+    }
+  }, []);
 
   return (
     <div
       id="gyroContain"
-      onClick={togglePlay}
       style={{
         position: "relative",
 
       }}
     >
-      <canvas 
-        ref={canvasRef} 
+      <div
         onClick={togglePlay}
-        style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", width: "100%", height: "100%" }} 
-      />
-      <video ref={videoRef} src="https://heartoss.xn--vuqw0e54ixuh2wab7xjjnvyb7x0m.online/video.mp4 " muted crossOrigin="anonymous"  style={{display: "none"}}  loop/>
+        style={{
+          position: "fixed",
+          top: "10px",
+          right: "20px",
+          cursor: "pointer",
+          zIndex: 1000,
+          width: "30px",
+          height: "30px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <img 
+          src={isPlaying ? "/pause.svg" : "/play.svg"} 
+          alt={isPlaying ? "暂停" : "播放"}
+          width="24"
+          height="24"
+        />
+      </div>
+      
+      <canvas ref={canvasRef} style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", width: "100%", height: "100%" }} />
+      <video ref={videoRef} src="https://heartoss.xn--vuqw0e54ixuh2wab7xjjnvyb7x0m.online/video.mp4 " muted crossOrigin="anonymous"  style={{display: "none"}} preload="auto" />
 
       <style jsx global>{`
-       
-
         @media screen and (orientation: portrait) {
           html {
             width: 100vmin;
